1aa2835b532db901fdc62b81ea1ce78130bd4b16
Hamza-amnikon <hamza.rajmohammed@amnikontechnologies.com>
2026-09-26 02:42:07 +0530

    Document

--- frontend/src/Admin/Settings/Profile/Docs.jsx
+++ frontend/src/Admin/Settings/Profile/Docs.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./MyDocuments.css";
+import "./Docs.css";
 import { useAuth } from "../../../context/AuthContext";
 
 /* ======================================================
--- frontend/src/Admin/Settings/SettingsDashboard.jsx
+++ frontend/src/Admin/Settings/SettingsDashboard.jsx
@@ -9,7 +9,7 @@ import ProfileInfo from "./Profile/ProfileInfo";
 import EmploymentInfo from "./Profile/EmploymentInfo";
 import ContactInfo from "./Profile/ContactInfo";
 import EmergencyContact from "./Profile/EmergencyContact";
-import Documents from "./Profile/Documents";
+import Documents from "./Profile/MyDocuments";
 
 import { getProfile } from "../Services/ProfileService";
 
